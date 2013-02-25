d4aaf98fb4bf9b293d26eb10dba9b5fdf360fd45
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-02-25 11:53:34 +0530

    implement mapReduce aggregator

---
+++ examples/minute.js
@@ -0,0 +1,22 @@
+var Aggregator = require('../lib/aggregator');
+
+var ag = new Aggregator('mongodb://localhost/metrics');
+var query = {
+	date: {
+		$gt: new Date('2013 02 22 4:00 UTC').getTime(),
+		$lt: new Date('2013 02 22 5:00 UTC').getTime()
+	}
+};
+
+ag.aggregate(query, 'minute', 'metrics', prt);
+
+function prt(err, results) {
+	
+	if(err) {
+		console.error(err);
+	} else {
+		results.find().each(function(err, doc) {
+			console.log(doc);
+		});
+	}
+}
